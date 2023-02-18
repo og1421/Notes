ec53cab2aed5ca027cbb70445838c61fb095c165
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-02-18 20:59:33 -0300

    Initial Commit

---
+++ Notes Watch App/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes Watch App
+//
+//  Created by Orlando Moraes Martins on 18/02/23.
+//
+
+import SwiftUI
+
+@main
+struct Notes_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
